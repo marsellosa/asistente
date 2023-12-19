ce3f2294a85486de27f277047c543c47065a8eb8
Marcelo Llosa <marcelo.llosa.m@gmail.com>
2023-12-18 23:42:57 -0400

    reportes actualizados

--- reportes/views.py
+++ reportes/views.py
@@ -8,15 +8,15 @@ from reportes.forms import ReporteDiarioForm
 from home.decorators import allowed_users
 
 @allowed_users(['admin', 'operadores'])
-def reporte_diario(request, id_operador):
+def reporte_diario(request, id_operador=None):
     context, template = {}, 'apps/reportes/list.html'
     
     date = request.GET.get('fechadesde')    
     if not date:
         date = get_today()
-    lista = Consumo.objects.by_id_operador(id_operador) #type: ignore
+    
+    lista = Consumo.objects.by_id_operador(id_operador) if type(id_operador) is int else Consumo.objects.all() #type: ignore
     consumos = lista.filter(comanda__fecha=date).order_by('pk')
-    # print(f"consumos: {consumos}")
     
     if request.htmx:
         if request.method == 'POST':
@@ -27,17 +27,23 @@ def reporte_diario(request, id_operador):
             except ValidationError:
                 messages.warning = (request, "faltan datos")
         template = 'apps/reportes/partials/results.html'
-    # print(f"sobre_rojo: {round(sum([item.sobre_rojo for item in consumos]), 2)}")
-    # print(f"mayoreo: {round(sum([item.mayoreo for item in consumos]), 2)}")    
-    # print(f"insumos: {round(sum([item.insumos for item in consumos]), 2)}")    
-    # print(f"puntos_volumen: {round(sum([item.puntos_volumen for item in consumos]), 2)}")    
-    # print(f"sobre_verde: {round(sum([item.sobre_verde for item in consumos]), 2)}")    
-    # print(f"total: {consumos.count()}")
+
     context = {
         'consumos': consumos,
         'totales': {
             'sobre_rojo': round(sum([item.sobre_rojo for item in consumos]), 2),
+            'mayoreo': round(sum([item.mayoreo for item in consumos]), 2),
+            'insumos': round(sum([item.insumos for item in consumos]), 2),
+            'insumos': round(sum([item.insumos for item in consumos]), 2),
+            'descuento': round(sum([item.descuento for item in consumos]), 2),
+            'sobre_verde': round(sum([item.sobre_verde for item in consumos]), 2),
+            'efectivo': round(sum([item.efectivo for item in consumos]), 2),
+            # 'efectivo_prepago': round(sum([item.monto for item in efectivo_prepago]), 2),
+            # 'efectivo_consumos': round(sum([item.efectivo for item in registrados]), 2),
+            # 'prepagos_x_id' : round(sum([item.monto for item in prepagos_operador]) ,2)
+
         },
         'form' : ReporteDiarioForm({'id': id_operador})
     }
     return render(request, template, context)
+
