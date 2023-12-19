1dac292006b040b28d013bc813d5fb7ca0f850f9
Marcelo Llosa <marcelo.llosa.m@gmail.com>
2023-12-19 00:09:24 -0400

    bug corregido

--- consumos/views.py
+++ consumos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.utils.timezone import datetime
 from consumos.models import Consumo
+from main.utils import get_today
 from operadores.models import Operador
 from reportes.forms import ReporteDiarioForm
 
@@ -11,10 +11,10 @@ def list_consumos_by_id_operador(request, id_operador):
     lista = Consumo.objects.by_id_operador(id_operador) #type:ignore
     date = request.GET.get('fechadesde')    
     if not date:
-        date = datetime.today()
+        date = get_today()
 
     consumos = lista.filter(inserted_on=date).order_by('-id')
-    # print(f"consumos: {consumos}, id_operador: {id_operador}")
+    
     context = {
         'operador': operador,
         'consumos': consumos,
