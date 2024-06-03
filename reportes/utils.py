b092e2fb85ae06bf11fd93a0d1ff09ca9bd3b6ac
Marcelo Llosa <marcelo.llosa.m@gmail.com>
2024-06-02 22:50:46 -0400

    datos agregados

--- reportes/utils.py
+++ reportes/utils.py
@@ -8,47 +8,31 @@ from main.utils import get_today
 
 def reporte_consumos(id_operador=None, fechaDesde=None, fechaHasta=None, user=None):
     context, consumos = {}, []
-    # lista = Consumo.objects.by_id_operador(id_operador) #type: ignore
+    
     if fechaDesde is None:
         fechaDesde = get_today().date()
-    # registrados = Consumo.objects.by_user(id_operador, fechaDesde) #type: ignore
-    # consumos = lista.filter(comanda__fecha=fechaDesde).order_by('-id')
-    # efectivo_prepago = Pago.objects.filter(fecha__date=fechaDesde, usuario__id=id_operador)
-    # prepagos_operador  = Pago.objects.filter(fecha__date=fechaDesde, prepago__socio__operador__id=id_operador).order_by('-fecha')
+
+    prepagos_operador  = Pago.objects.filter(fecha__date=fechaDesde, prepago__socio__operador__id=id_operador).order_by('-fecha')
         
     rango = (
         fechaDesde is not None and
         fechaHasta is not None
     )
     
-    print(f"id_operador: {type(id_operador)}")
     if rango:
-        print(f"con rango: {fechaDesde} hasta: {fechaHasta}")
-        consumos = Consumo.objects.filter(comanda__fecha__gte=fechaDesde, comanda__fecha__lte=fechaHasta).order_by('-comanda__fecha')
+        consumos = Consumo.objects.by_date_range(fechaDesde, fechaHasta) #type: ignore
+        # consumos = Consumo.objects.filter(comanda__fecha__gte=fechaDesde, comanda__fecha__lte=fechaHasta).order_by('-comanda__fecha')
         efectivo_prepago = Pago.objects.filter(fecha__date__gte=fechaDesde, fecha__date__lte=fechaHasta)
-        
     else:
-        print(f'sin rango: {fechaDesde}')
-        # consumos = Consumo.objects.filter(comanda__fecha=fechaDesde).order_by('-comanda__fecha')
         consumos = Consumo.objects.by_date(fechaDesde) # type: ignore
-        
         efectivo_prepago = Pago.objects.filter(fecha__date=fechaDesde)
-    # if user is not None:
-    #     prepagos_x_usuario = efectivo_prepago.filter(usuario=user)
-    #     print(prepagos_x_usuario)
+
     if type(id_operador) is int:
-        # consumos = consumos.filter(comanda__socio__operador__id=id_operador)
-        # print(f"usuario: {Operador.objects.get(id=id_operador).licencia.persona.usuario_set.all().first()}")
         consumos = consumos.by_id_operador(id_operador) # type: ignore
         efectivo_prepago = efectivo_prepago.filter(prepago__socio__operador__id=id_operador)
-        # efec_prepago_by_operador =efectivo_prepago.filter(prepago__socio__operador__id=id_operador)
-    print(consumos)
-    # context = {
-    #     'consumos': consumos,
-    # }
-    print(f"total_consumido: {round(sum([item.total_consumido for item in consumos]), 2)}")
+
     context = {
-        'id_operador': id_operador,
+        'operador': Operador.objects.get(pk=id_operador),
         'consumos': consumos,
         'hoy': fechaDesde,
         'totales': {
@@ -62,7 +46,7 @@ def reporte_consumos(id_operador=None, fechaDesde=None, fechaHasta=None, user=No
             'efectivo': round(sum([item.efectivo for item in consumos]), 2),
             'efectivo_prepago': round(sum([item.monto for item in efectivo_prepago]), 2),
             'efectivo_consumos': round(sum([item.efectivo for item in consumos]), 2),
-    #         'prepagos_x_id' : round(sum([item.monto for item in prepagos_operador]) ,2)
+            'prepagos_x_id' : round(sum([item.monto for item in prepagos_operador]) ,2)
         },
     }
     
