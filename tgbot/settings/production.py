ec804101663b5d1a53135c85df5723173c6ee2e0
Marcelo Llosa <marcelo.llosa.m@gmail.com>
2024-06-09 13:39:09 -0400

    depurando

--- tgbot/settings/production.py
+++ tgbot/settings/production.py
@@ -8,7 +8,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-d293.up.railway.app', 'miasistenteherbalife.com']
+ALLOWED_HOSTS = ['*']
 
 PGDATABASE   = config('PGDATABASE', default=None)
 PGPASSWORD   = config('PGPASSWORD', default=None)
