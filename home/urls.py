0f2c1203d5777bb12beb44c1ef00b1c59e374e89
Marcelo Llosa <marcelo.llosa.m@gmail.com>
2024-11-14 23:23:33 -0400

    recuperar contraseña completa

--- home/urls.py
+++ home/urls.py
@@ -1,16 +1,31 @@
 from django.contrib.auth.views import * #type:ignore
-from django.urls import path
+from django.urls import path, reverse_lazy
 from home.views import *
 
-app_name = 'home'
+# app_name = 'home'
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('register/', register_view, name='register'),
-    path('reset_password/', PasswordResetView.as_view(template_name='apps/home/forgot_password.html'), name='reset_password'),
-    path('reset_password_sent/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='apps/home/recover_password.html'), name='password_reset_confirm'),
-    path('reset_password_complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('hx/password_check_match/', check_password_match, name='check_password_match'),
+    path('password_reset/', PasswordResetView.as_view(
+            template_name='apps/home/forgot_password.html', 
+            email_template_name='apps/home/password_reset_email.html',
+            subject_template_name='apps/home/password_reset_subject.txt',
+        ), 
+        name='password_reset'
+    ),
+    path('password_reset/done/', PasswordResetDoneView.as_view(
+            template_name='apps/home/password_reset_done.html',
+        ), 
+        name='password_reset_done'
+    ),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
+            template_name='apps/home/password_reset_confirm.html',
+            success_url=reverse_lazy('password_reset_complete')
+        ), 
+        name='password_reset_confirm'),
+    path('password_reset_complete/', PasswordResetCompleteView.as_view(template_name='apps/home/password_reset_complete.html'), name='password_reset_complete'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse, Http404, HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -36,12 +37,25 @@ def register_view(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('email')
-            messages.success(request, f"Account was created for {username}")
-            return redirect('home:login')
+            messages.success(request, f"Se creó la cuenta para {username}")
+            return redirect('login')
 
     context['form'] = form
     return render(request, template, context)
 
 def logout_view(request):
     logout(request)
-    return redirect('home:login')
+    return redirect('login')
+
+def check_password_match(request):
+    context, template = {}, 'apps/home/partials/password_input.html'
+    if not request.htmx:
+        raise Http404
+    
+    password1 = request.POST.get("password1", "")
+    password2 = request.POST.get("new_password2", "")
+    
+    if password1 != password2:
+        return HttpResponse('<p>Las contraseñas no coinciden.</p>')
+    
+    return HttpResponse('')
