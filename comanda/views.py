9b8cb4ead290ee847525f5f9b1ea6246d157b37c
Marcelo Llosa <marcelo.llosa.m@gmail.com>
2024-12-02 19:32:55 -0400

    bug corregido

--- comanda/views.py
+++ comanda/views.py
@@ -11,14 +11,14 @@ from socios.models import Socio
 from productos.models import *
 
 User = settings.AUTH_USER_MODEL
-@allowed_users(['admin', 'operador'])
+@allowed_users(['admin', 'operadores'])
 def comanda_view(request):
     context, template = {}, 'apps/comanda/partials/total.html'
     prod = get_object_or_404(Categoria, nombre='BATIDO')
     context = {'prod': prod}
     return render(request, template, context)
     # return HttpResponse(prod)
-@allowed_users(['admin', 'operador'])
+@allowed_users(['admin', 'operadores'])
 def hx_create_comanda_view(request, id_socio=None):
     context, template = {}, 'apps/comanda/partials/comanda-pendiente.html'
     
@@ -34,7 +34,7 @@ def hx_create_comanda_view(request, id_socio=None):
             }
     
     return render(request, template, context)
-@allowed_users(['admin', 'operador'])
+@allowed_users(['admin', 'operadores'])
 def hx_crud_comandaitem_view(request, id_comanda=None, id_receta=None):
     # add/delete item for Comanda
     context, template = {}, 'apps/comanda/partials/table-form.html'
@@ -93,7 +93,7 @@ def hx_crud_comandaitem_view(request, id_comanda=None, id_receta=None):
     #     'msg': msg
     # }
     return render(request, template, context)
-@allowed_users(['admin', 'operador'])
+@allowed_users(['admin', 'operadores'])
 def hx_add_prepago_view(request, id_comanda=None, id_prepago=None):
     context, template = {}, 'apps/comanda/partials/total.html'
     
@@ -113,7 +113,7 @@ def hx_add_prepago_view(request, id_comanda=None, id_prepago=None):
     prepago.save()
     context = {'parent_obj': comanda}
     return render(request, template, context)
-@allowed_users(['admin', 'operador'])
+@allowed_users(['admin', 'operadores'])
 def comanda_item_edit_view(request, id_comanda_item):
     context, template = {}, 'apps/comanda/partials/comanda-item.html'
     if not request.htmx:
@@ -134,7 +134,7 @@ def comanda_item_edit_view(request, id_comanda_item):
     context['obj']= comanda_item
 
     return render(request, template,context)
-@allowed_users(['admin', 'operador'])
+@allowed_users(['admin', 'operadores'])
 def hx_update_status(request, id_comanda):
     context, template = {}, 'apps/pedidos/partials/item-deleted.html'
     comanda = get_object_or_404(Comanda, id=id_comanda)
