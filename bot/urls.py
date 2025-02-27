d0c53342390edaf626aff7b89ec2f53339dbb4d8
Marcelo Llosa <marcelo.llosa.m@gmail.com>
2025-02-27 19:56:40 -0400

    corrigiendo bugs

--- bot/urls.py
+++ bot/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from bot.views import TelegramWebhookView
+from bot.views import *
 
 app_name = 'bot'
 
 urlpatterns = [
-    path('', csrf_exempt(TelegramWebhookView.as_view()), name='update'),
-    # path('send/<str:user_id>/', user_send_message, name='send_message'), #type:ignore
-    # path('send/<str:user_id>/pedido/<str:pedido_id>/', enviar_detalle_pedido, name='send_pedido'), #type:ignore
+    # path('', csrf_exempt(TelegramWebhookView.as_view()), name='update'),
+    path('send/<str:user_id>/', user_send_message, name='send_message'), #type:ignore
+    path('send/<str:user_id>/pedido/<str:pedido_id>/', enviar_detalle_pedido, name='send_pedido'), #type:ignore
 ]
